dc9f6c75f3a7ca4104c9b4676315ef9c9472ed35
AsyaGom <anastasia.gomonova1@gmail.com>
2018-05-04 20:56:00 +0300

    Done lab4

--- 6383/GomonovaAA/lab4/lab4.asm
+++ 6383/GomonovaAA/lab4/lab4.asm
@@ -1,299 +1,245 @@
+.286
 
-CODE	SEGMENT
-		keep_1c		dd 0 ;переменная для хранения сегмента и смещения старого прерывания 1c
-		keep_2f		dd 0 ;переменная для хранения сегмента и смещения старого прерывания 2f
-		keep_PSP   	dw ? 
-        ASSUME CS:CODE, DS:DATA, SS:AStack
+CODE SEGMENT
+	ASSUME CS:CODE, DS:DATA, SS:STACK_S	
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	TETR_TO_HEX		PROC near
+		and  AL, 0Fh
+		cmp  AL, 09h
+		jbe  NEXT
 		
-; функция вывода в текущее положение курсора символа из AL
-outputAL 		PROC 
-		push AX
-		push BX
+		add  AL, 07h
+NEXT:	add  AL, 30h
+		ret
+TETR_TO_HEX		ENDP
+;--------------------;
+
+; AL ---> AX
+BYTE_TO_HEX		PROC near
 		push CX
-	
-		mov AH, 09h
-		mov BH, 0
-		mov CX, 1
-		int 10h
-
+		
+		mov  AH, AL
+		call TETR_TO_HEX
+		xchg AL, AH
+		mov  CL, 04h
+		shr  AL, CL
+		call TETR_TO_HEX
+						
 		pop CX
-		pop BX
-		pop AX
 		ret
-outputAL 		ENDP
+BYTE_TO_HEX		ENDP
+;------------------------;
 
-;функция считывания позиции курсора: DH - текущая строка, DL - колонка курсора
-SaveCurs  PROC
-		push AX
+;DI
+WRD_TO_HEX		PROC near
 		push BX
-
-		mov AH, 03h
-		mov BH, 0
-		int 10h
-
-		pop BX
-		pop AX
-		ret
-SaveCurs  ENDP
-
-;функция, устанавливающая курсор в заранее определённую в DX позицию
-SetCurs  PROC
-		push AX
-		push BX
-
-		mov AH, 02h
-		mov BH, 0
-		int 10h
 		
-		pop BX
-		pop AX
+		mov  BH, AH
+		call BYTE_TO_HEX
+		mov  ES:[DI], AH
+		dec  DI
+		mov  ES:[DI], AL
+		dec  DI
+		mov  AL, BH
+		call BYTE_TO_HEX
+		mov  ES:[DI], AH
+		dec  DI
+		mov  ES:[DI], AL
+		
+		pop  BX
 		ret
-SetCurs  ENDP
+WRD_TO_HEX		ENDP
+;-------------------------------;
 	
-;собственный обработчик прерывания для 2F
-My_2F	PROC
-		cmp	 AH, 080h ;сравниваем значение в AH с установленным ранее перед прерыванием 
-		jne  not_loaded ;если значения не равны - программа не установлена резидентной в памяти
-		mov  AL, 0FFh ;устанавливаем в AL значение FF, что на выходе из прерывания означает, что программа установлена 
-	not_loaded:
-		iret ;иначе просто возвращаемся в программу
-My_2F	ENDP
+INTERRUPT	PROC far
+mov  CS:KEEP_AX, AX
+mov  CS:KEEP_SS, SS
+mov  CS:KEEP_SP, SP
 
-;собственный обработчик прерывания для 1C
-My_1C	PROC
-		push AX
-		push BX
-		push CX
-		push DX
-		push ES
+mov  AX, SEG NEW_STACK
+mov  SS, AX
+mov  SP, offset QWE
 
-		inc count
-		cmp count, 57 ;диапазон символов [0..9] = [48..57]
-		jne show
-		mov count, 48
-	show:
+	pusha
+	push ES
 		
-		; Сохраняем текущее положение курсора
-		call SaveCurs
-		mov CX, DX
-
-		; Переносим курсор в указаную позицию и выводим туда символ
-		mov DH, 23
-		mov Dl, 33
-		call SetCurs
-		push AX
-		mov AL, count
-		call OutputAL
-		pop AX
+		inc CS:COUNTER
 		
-		; Возвращаем курсор
-		mov DX, CX
-		call SetCurs
-
-		mov AL, 20h
-		out 20h, AL
-
-		pop	ES
-		pop DX
-		pop CX
-		pop BX
-		pop AX
-		iret 	
-LAST_BYTE:
-My_1C 	ENDP
-
-; функция проверки не ввёл ли пользователь команду /un
-Un_check  PROC	FAR
-		push AX
+		mov  AX, CS
+		mov  ES, AX
 		
-		mov	AX, Keep_PSP
-		mov	ES, AX
-		sub	AX, AX
-		cmp	byte ptr es:[82h],'/'
-		jne	not_un
-		cmp	byte ptr es:[83h],'u'
-		jne	not_un
-		cmp	byte ptr es:[84h],'n'
-		jne	not_un
-		mov	flag,0
+		mov  AH, 03h				; определяем
+		mov  BH, 0h					; текущее положение
+		int  10h					; курсора
+		push DX				; тек. строка и колонка
+		push CX				; тек. нач. и конеч. строки
 		
-	not_un:
-		pop	AX
-		ret
-Un_check  ENDP
-
-;функция, сохраняющая стандартные обработчики прерываний
-Keep_interr	 PROC
-		push AX
-		push BX
-		push ES
-
-		mov AH, 35h ;функция, выдающая значение сегмента в ES, смещение в BX
-		mov AL, 1Ch ;для прерывания 1C
-		int 21h
-		mov word ptr keep_1c, BX
-		mov word ptr keep_1c+2, ES
+		mov  AX, CS:COUNTER			; помещаем 
+		mov  DI, offset STRING + 20	; счетчик в
+		call WRD_TO_HEX				; строку
 		
-		mov AH, 35h ;функция, выдающая значение сегмента в ES, смещение в BX
-		mov AL, 2Fh ;для прерывания 2F
-		int 21h	
-		mov word ptr keep_2f, BX
-		mov word ptr keep_2f+2, ES
-
-		pop ES
-		pop BX
-		pop AX
-		ret
-Keep_interr	 ENDP
-
-; функция, загружающая собственные обработчики прерывания
-Load_interr	 PROC
-		push DS
-		push DX
-		push AX
-
-		call Keep_interr ;сохраняем старые обработчики прерываний
-
-		push DS
-		mov DX, offset My_1C
-		mov AX, seg My_1C	    
-		mov DS, AX
-		mov AH, 25h		 ;функция, меняющая обработчик прерываний на указанный в DX и AX
-		mov AL, 1Ch      ;для прерывания 1C         	
-    	int 21h
-
-    	mov DX, offset My_2F
-		mov AX, seg My_2F	    
-		mov DS, AX
-		mov AH, 25h		 ;функция, меняющая обработчик прерываний на указанный в DX и AX   
-		mov AL, 2Fh      ;для прерывания 2F       	
-    	int 21h	
-		pop DS
+		mov  AH, 13h				;	
+		mov  AL, 1h					; исп. атр. BL; не трогать курсор
+		mov  BH, 0h					; видео страница
+		mov  BL, 99h
+		mov  CX, 21					; символов в строке
+		mov  DH, 22					; строка курс.
+		mov  DL, 40					; колонка курс.
+		mov  BP, offset CS:STRING	; печат. строка
+		int  10h					; 
+		
+		pop  CX				; вост. положение
+		pop  DX				; курсора
+		mov  AH, 02h		;
+		int  10h			;
+		
+	pop  ES
+	popa
 	
-		pop AX
-		pop DX
-		pop DS
-		ret
-Load_interr  ENDP
+mov  AX, CS:KEEP_SS
+mov  SS, AX
+mov  SP, CS:KEEP_SP
 
-; Выгружаем обработчики прерываний
-Unload_interr  PROC
-		push DS
+	mov al, 20h
+	out 20h, al
+	
+mov  AX, CS:KEEP_AX
 
-		mov AH, 35h
-		mov AL, 1Ch
-		int 21h
-		mov DX, word ptr es:keep_1c
-		mov AX, word ptr es:keep_1c+2
-		mov word ptr keep_1c, DX
-		mov word ptr keep_1c+2, AX
+	iret	
+	
+	nop
+	INT_KEY		db 'sfsdgihodh;9/8652lhkadfglhkjfgjgdxh/86451!', 0ah, '$'
+	STRING		db 'Number of calls:     '
+	KEEP_CS		dw 0h
+	KEEP_IP		dw 0h
+	KEEP_PSP	dw 0h
+	COUNTER		dw 0h
+	
+	KEEP_SP		dw 0h
+	KEEP_SS		dw 0h
+	KEEP_AX		dw 0h
+	
+	NEW_STACK	db 20h DUP(0)
+	
+INTERRUPT	ENDP
+QWE:		
+	
+	
+MAIN PROC far
+	push DS
+		mov  CS:KEEP_PSP, DS
+	and  AX, 0
+	push AX
+	mov  AX, DATA
+	mov  DS, AX
+	
 
-		mov AH, 35h
-		mov AL, 2Fh
-		int 21h
-		mov DX, word ptr es:keep_2f
-		mov AX, word ptr es:keep_2f+2
-		mov word ptr keep_2f, DX
-		mov word ptr keep_2f+2, AX
+	mov  ah, 35h					; функция получения вектора
+	mov  al, 1Ch					; номер вектора
+	int  21h						; ES:BX
+	
+	
+	mov  DI, BX						; подготовка к
+	mov  DI, offset ES:INT_KEY		; проверке на
+	mov  SI, offset KEY				; пользовательское 
+	mov  CX, 42						; прерывание
+	
+	repe cmpsb						;
+	cmp  CX, 0						; проверка на польз.
+	jz   nqwe 						; прерывание
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	mov  DX, offset INT_SET
+	mov  AH, 09h
+	int  21h
 
-		CLI
-		mov DX, word ptr keep_1c
-		mov AX,	word ptr keep_1c+2
-		mov DS, AX
-		mov AH, 25h	;выгружаем обработчик для 1C
-		mov AL, 1Ch
-		int 21h
-		
-		mov DX, word ptr keep_2f
-		mov AX,	word ptr keep_2f+2
-		mov DS, AX
-		mov AH, 25h	;выгружаем обработчик для 2F
-		mov AL, 2Fh
-		int 21h
-		STI
-		
-		pop DS
+	mov  CS:KEEP_IP, BX
+	mov  CS:KEEP_CS, ES
 
-		mov ES, ES:Keep_PSP
-		mov AX, 4900h		;освобождаем память по адресу ES:Keep_PSP
-		int 21h
-		
-		mov flag, 1			;запоминаем, что память была освобождена
-		mov DX, offset Message2
-		call Write_message  ;выводим соответствующее сообщение
+	push DS
+		mov  DX, offset INTERRUPT 	; смещение для прерывания в DX
+		mov  AX, SEG    INTERRUPT 	; сегмент прерывания	
+		mov  DS, AX				  	; помещаем в DS
+		mov  AH, 25h 			  	; функция установки вектора
+		mov  AL, 1Ch 			  	; номер вектора
+		int  21h 				  	; меняем прерывание
+	pop  DS
+	
+	
+	
+	mov  DX, offset QWE				;
+	shr  DX, 4						;
+	inc  DX							;
+	add  DX, CODE					; воход в DOS
+	sub  DX, CS:KEEP_PSP			; с сохранением
+	mov  AH, 31h					; программы
+	int  21h						; в памяти
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+	
+nqwe:
+	push ES
+		mov  ES, CS:KEEP_PSP		; подготовка к
+		mov  DI, 82h				; проверке
+		mov  SI, offset TCL_KEY		; хвоста
+		mov  CX, 3					; командной строки
+	
+		repe cmpsb					; проверка 
+		cmp  CX, 0					; хвоста
+		jne  alr_inst				; командной строки
+	pop  ES
+	
+	mov  DX, offset INT_RES
+	mov  AH, 09h
+	int  21h	
 
-		mov ES, ES:[2ch]	
-		mov AX, 4900h		;освобождаем память по адресу ES:[2ch]
-		int 21h
+	
+	CLI
+	push DS
+		mov  DX, ES:KEEP_IP
+		mov  AX, ES:KEEP_CS
+		mov  DS, AX
+		mov  AH, 25h 				; функция установки вектора
+		mov  AL, 1Ch 				; номер вектора
+		int  21h 					; меняем прерывание
+	pop  DS
+	STI
+	
+	mov  ES, ES:KEEP_PSP			; Начинаем удалять прерывание + его среду
+	
+	push ES							
+		mov  ES, ES:[2Ch]			;
+		mov  AH, 49h  				;
+		int  21h					; удаление среды резидентного прерывания
+	pop  ES						
+	int  21h						; удаление  резидентной программы
 
-		mov AX, 4C00h		;выход из программы через функцию 4C
-		int 21h
-Unload_interr  ENDP
+	jmp exit
+	
+alr_inst:
+	mov  DX, offset INT_ALR		; Custom 
+	mov  AH, 09h				; interrupt
+	int 21h						; already installed
+	
+exit:
+	mov  AH, 4Ch
+	int  21h
 
-Make_resident  PROC
-		mov AX, ES
-		mov Keep_PSP, AX
-		mov DX, offset LAST_BYTE
-		add DX, 200h	
-		
-		mov AH, 31h ;31h завершает программу, оставляя её резидентной в памяти
-		mov AL, 0 
-		int 21h
-Make_resident  ENDP
+MAIN ENDP
 
-; функция вывода сообщения на экран
-Write_message	PROC
-		push AX
-		mov AH, 09h
-		int 21h
-		pop AX
-		ret
-Write_message		ENDP
+CODE ENDS
 
-; Главная функция
-Main 	PROC  
-		push DS
-		xor AX, AX
-		push AX
-   		mov AX, DATA             
-  		mov DS, AX
-		mov Keep_PSP, ES 
-		mov Count, 48
+DATA SEGMENT
+	KEY		db 'sfsdgihodh;9/8652lhkadfglhkjfgjgdxh/86451!', 0ah, '$'
+	INT_ALR	db 'Custom interrupt already installed', 0Ah, '$'
+	INT_SET	db 'Setting the custom interrupt', 0Ah, '$'
+	INT_RES	db 'Restore system interrupt', 0Ah, '$'
+	TCL_KEY	db '/un'
+DATA ENDS
 
-		mov	AX, 8000h ;нам нужны номера в AH от 80h до 0FFh
-		int 2Fh
-		cmp	AL,0FFh	  ; 2fh возвращает 0FFh, если программа установлена резидентной в памяти
-		jne loading
+STACK_S SEGMENT STACK
+	DW 100h DUP(?)
+STACK_S ENDS
 
-		call Un_check
-		cmp flag, 0
-		jne alr_loaded
-
-		call Unload_interr	;пользователь ввёл /un и программа ещё не была выгружена
-	loading:				;программа не является резидентной в памяти
-		call Load_interr
-		
-		lea DX, Message1
-		call Write_message
-		
-		call Make_resident
-	alr_loaded:				;программа уже была резидентной
-		lea dx, Message3
-		call Write_message
-		mov ax, 4C00h
-		int 21h
-Main 	ENDP
-CODE    		ENDS
-
-AStack		SEGMENT  STACK
-        DW 256 DUP(?)			
-AStack  	ENDS
-
-DATA		SEGMENT
-	Count 	 		db ?
-    flag			dw 1
-    Message1        db 'Resident program has been loaded', 0dh, 0ah, '$'
-    Message2	    db 'Resident program unloaded', 0dh, 0ah, '$'
-    Message3		db 'Resident program is already loaded', 0dh, 0ah, '$'
-DATA 		ENDS
-        	END Main
+END MAIN
